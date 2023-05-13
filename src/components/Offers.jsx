bf62d460091d30463a4aaa46432bf3daec93e3f8
carlo <mohammadabbasi378@gmail.com>
2023-05-13 11:00:16 +0330

    eddit readme file

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -6,7 +6,7 @@ const Offers = () => {
       <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">
         Our Offers
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <div className="p-6">
             <h3 className="text-lg font-bold mb-2 text-gray-800">
